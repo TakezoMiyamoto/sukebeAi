555185affbb0090978201e244bee4de2d3dcc737
takezo <takezo.m.1987@gmail.com>
2018-08-20 12:21:10 +0900

    add codes in faceAPI.py

--- faceAPI.py
+++ faceAPI.py
@@ -20,7 +20,7 @@ def makeGroup():
   )
   print(r.text)
 
-  def makePerson(personName):
+def makePerson(personName):
     # makeGroupで作為性したgroupにpersonを追加する
     end_point = BASE_URL + "persongroups" + GROUP_NAME + "/persons"
     headers = {
@@ -42,7 +42,7 @@ def makeGroup():
     return personId
 
   # 画像をpersonにセットする
-  def addFaceToPerson(personId, imageUrl):
+def addFaceToPerson(personId, imageUrl):
     """
     PersonにFaceを追加
     params:
@@ -75,7 +75,95 @@ def makeGroup():
       print("personId is not set.")
       return None
 
+# Person と Person Faceを一致させるために画像を学習させる
+def trainGroup(groupId):
+  end_point = BASE_URL + "persongroups/" + GROUP_NAME + "/train"
+  headers = {
+    "Ocp_Apim-Subscription-Key" :SUBSCRIPTION_KEY
+  }
+  r = requests.post(
+      end_point,
+      headers = headers,
+  )
+  print(r.text)
 
+# 画像のURLを送信すると、その中の顔を検知してFaceIdを返す
+def detectFace(imageUrl):
+  """
+  学習済みのpersonGroupの中で、送信する画像のURLから似ている候補を取得できる
+  """
+  end_point = BASE_URL + "detect"
+  headers = {
+    "Ocp_Apim-Subscription-Key" :SUBSCRIPTION_KEY
+  }
+  payload = {
+    "url": imageUrl
+  }
+  r = requests.post(
+      end_point,
+      json = payload,
+      headers = headers
+  )
+  try:
+    faceId = r.json()[0]["faceId"]
+    print("faceId Found:{}".format(faceId))
+    return r.json()[0]
+  except Exception as e:
+    print("faceId no found:{}".format(e))
+    return None
+
+#Fade IDをもとに、学習された画像を特定する。一致する女優がJSON形式で帰ってくる。
+
+def identifyPerson(faceId):
+  end_point = BASE_URL + "identify"
+  headers = {
+    "Ocp_Apim-Subscription-Key" :SUBSCRIPTION_KEY
+  }
+  faceIds = [faceId]
+  payload = {
+    "faceIds" :faceIds,
+    "personGroupId" :GROUP_NAME,
+
+  }
+  r = requests.post(
+      end_point,
+      json = payload,
+      headers = headers
+  )
+  print(r.text)
+
+
+if __name__ = '__main__':
+  #pandasでcsvからAV女優を学習させます
+  #AV女優のリストを取得する
+  df = pd.read_csv("romaned.csv",index_col=0)
+  df2 = pd.read_csv("learning-default.csv", index_col=0)
+
+  for i, row in df.iterrows(): #,name,kana,image,dmmimage,roman
+    name = row["name"]
+    kana = row["kana"]
+    image = row["image"]
+    dmmimage = row["dmmimage"]
+    #personを登録し、personIdを返す
+    personId = makePerson(name)
+    #personIdをもとに、そのpersonにスクレイピングした画像とDMMの画像を追加する。
+    addFaceToPerson(personId, dmmimage)
+    addFaceToPerson(personId, image)
+    se = pd.Series([name,kana,image,dmmimage,personId],["name","kana","image","dmmimage","personId"])
+    df2 = df2.append(se,ignore_index=True)
+    print(df2)  
+
+  #画像からpersonを特定するときのサンプルコード
+  image = "https://cdn.mainichi.jp/vol1/2016/09/02/20160902ddm001010041000p/9.jpg?1"
+  faceId = detectFace(image)
+  person = identifyPerson(faceId["faceId"])
+  if person["candidates"]: #学習データに候補があれば
+      personId = person["candidates"][0]["personId"]
+      personInfo = getPersonInfoByPersonId(personId)
+      print(personInfo["name"])
+  else:
+      print("No candidates found")
+      
 
 
 
